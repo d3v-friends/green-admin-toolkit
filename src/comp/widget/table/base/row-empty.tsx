be21ac9f0c6fb9c33bab0009f5a9be85c5c97d52
ciaolee <ciaolee@d3v-friends.com>
2025-06-24 16:54:22 +0900

    0.0.59

--- src/comp/widget/table/base/row-empty.tsx
+++ src/comp/widget/table/base/row-empty.tsx
@@ -10,7 +10,7 @@ export default function <T>({children, cols}: Readonly<Props<T>>) {
 	return (
 		<tr className="border-top">
 			<td
-				className="text-center pt-2 pb-2 h-15"
+				className="pt-2 pb-2 h-15"
 				colSpan={cols.length}>
 				{children || "내용이 없습니다."}
 			</td>
