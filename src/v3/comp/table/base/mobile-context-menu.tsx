9316684ff1d5178cefd1c7948f55fb6d995afbc8
ciaolee87 <ciaolee87@d3v-friends.com>
2026-03-16 18:07:08 +0900

    - GTable > mobile-context-menu 에니메이션 효과 추가

--- src/v3/comp/table/base/mobile-context-menu.tsx
+++ src/v3/comp/table/base/mobile-context-menu.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, {ReactNode} from "react";
+import React, {ReactNode, useEffect, useRef, useState} from "react";
 import {FnVoid} from "nextjs-tools";
 import {createRoot} from "react-dom/client";
 
@@ -31,16 +31,38 @@ export default function <T>({children, row}: Readonly<Props<T>>) {
 				if (e.pointerType !== "touch") return;
 				onClose();
 			}}>
-			<div
-				autoFocus
-				className="table-mobile-context-menu"
-				onPointerDown={(e) => e.stopPropagation()}
-				onContextMenu={(e) => e.preventDefault()}>
-				<div className="flex justify-center mb-1">
-					<div className="table-mobile-context-menu-bar" />
-				</div>
-				{children(row, onClose)}
+			<Menu>{children(row, onClose)}</Menu>
+		</div>
+	);
+}
+
+interface MenuProps {
+	children: ReactNode;
+}
+
+const MenuStyle: Record<string, string> = {
+	open: "table-mobile-context-menu translate-y-0",
+	close: "table-mobile-context-menu translate-y-full",
+};
+
+function Menu({children}: Readonly<MenuProps>) {
+	const contRef = useRef<HTMLDivElement>(null);
+	const [isOpen, onChangeOpen] = useState(false);
+	useEffect(() => {
+		onChangeOpen(true);
+	}, []);
+
+	return (
+		<div
+			ref={contRef}
+			autoFocus
+			className={MenuStyle[isOpen ? "open" : "close"]}
+			onPointerDown={(e) => e.stopPropagation()}
+			onContextMenu={(e) => e.preventDefault()}>
+			<div className="flex justify-center mb-1">
+				<div className="table-mobile-context-menu-bar" />
 			</div>
+			{children}
 		</div>
 	);
 }
